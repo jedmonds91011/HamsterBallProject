c798ff20593de81d6bc153a9a27b98d523ea598f
Joshua Edmonds <jedmonds91011@yahoo.com>
2014-04-22 22:37:40 -0700

    fucking colliders man...
    Shit sucks...

---
+++ Hamster/Assets/Scripts/Reza's Scipts/MagneticWall.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagneticWall : MonoBehaviour {
+
+	public GameObject character;
+	public Texture killTex;
+	
+	void OnCollisionEnter()
+	{
+		if( character.renderer.material.mainTexture == killTex )
+			{
+			Debug.Log("You Died");
+			//character.SetActive(false);
+			character.rigidbody.AddExplosionForce(1000.0f, gameObject.transform.position, 100.0f);
+			}
+	}
+}
